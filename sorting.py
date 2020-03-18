8cd734d0d33a308cd9e7bf7f8b8188c1f9180c7f
Marcin Bloch <marcin.bloch@appeartv.com>
2020-03-18 14:43:28 +0100

    Implement bubble sort

--- sorting.py
+++ sorting.py
@@ -28,6 +28,31 @@ class SelectionSort:
         a_collection[a_idx] = tmp
         self.num_swaps += 1
 
-    def num_swaps_(self):
+    def num_swaps(self):
         return self.num_swaps
 
+
+class BubbleSort:
+    def __init__(self):
+        self.num_swaps = 0
+
+    def sort(self, a_collection):
+
+        size = len(a_collection) - 1
+        for i in range(len(a_collection) - 1):
+            for i in range(size):
+                if a_collection[i] > a_collection[i+1]:
+                    tmp = a_collection[i]
+                    a_collection[i] = a_collection[i+1]
+                    a_collection[i+1] = tmp
+                    self.num_swaps += 1
+            size -= 1
+
+    def num_swaps(self):
+        return self.num_swaps
+
+
+a = [5, 1, 2, 3, 7, 8, 6, 4]
+b = BubbleSort()
+b.sort(a)
+print(a)
